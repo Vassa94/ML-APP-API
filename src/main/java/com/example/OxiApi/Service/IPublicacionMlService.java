9d1f90932da413a638d0d5ca366ce78c8c5eb12c
Vassa94 <vassallo1994@gmail.com>
2023-04-25 17:00:13 -0300

    Correccion visual para mejor lectura

--- src/main/java/com/example/OxiApi/Service/IPublicacionMlService.java
+++ src/main/java/com/example/OxiApi/Service/IPublicacionMlService.java
@@ -5,8 +5,11 @@ import com.example.OxiApi.Model.PublicacionMl;
 import java.util.List;
 
 public interface IPublicacionMlService {
-    public List<PublicacionMl> getPublicacionMl ();
+    public List<PublicacionMl> getPublicacionMl();
+
     public void savePublicacionMl(PublicacionMl publicacionMl);
-    public void deletePublicacionMl (Long id);
-    public PublicacionMl findPublicacionMl(Long id);
+
+    public void deletePublicacionMl(String id);
+
+    public PublicacionMl findPublicacionMl(String id);
 }
